68e90332aec810db6ee9bf5efa8bfb73efb9b044
jackmpauly <pauly@chapman.edu>
2022-11-16 16:57:33 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "password",
+    database = "NationalParks"
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE NationalParks")
+
+mycursor.execute("DROP TABLE customers")
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
