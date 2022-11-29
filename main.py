d663406e36690d0860c9c30e588d86374efa4bf0
jackmpauly <pauly@chapman.edu>
2022-11-28 16:57:44 -0800

    added createTables.py with 2 methods

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import mysql.connector
+import csv
+import createTables
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "password",
-    database = "NationalParks"
-)
+def main():
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "password",
+        database = "NationalParks"
+    )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE NationalParks")
+    # mycursor.execute("CREATE DATABASE NationalParks")
 
-mycursor.execute("DROP TABLE customers")
+    mycursor.execute("DROP TABLE customers")
 
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+    mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
+    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+    val = ("John", "Highway 21")
+    mycursor.execute(sql, val)
 
-mycursor.execute("SELECT * FROM customers")
+    mycursor.execute("SELECT * FROM customers")
 
-myresult = mycursor.fetchall()
+    myresult = mycursor.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
+
+
+    mycursor.execute("DROP TABLE state_province")
+    mycursor.execute("DROP TABLE country")
+    
+    createTables.country(mycursor)
+    createTables.state_province(mycursor)
+
+main()
